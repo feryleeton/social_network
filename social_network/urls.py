8b25dda349bbe66d89ad9a41d1737a671e804c5a
Pavlo Loboda <p.loboda@coi.ua>
2022-10-17 14:38:49 +0300

    feat: added drf_yasg doc

--- social_network/urls.py
+++ social_network/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from social_network import settings
+from .yasg import urlpatterns as doc_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,3 +13,4 @@ urlpatterns = [
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += doc_urlpatterns
